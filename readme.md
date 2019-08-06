f095d164e2065f59f58b0f343741321652be7206
vagrant <vagrant@vagrant.vm>
2019-08-06 20:56:13 +0000

    Adding Readme and Final work on prototype

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Cheapo Aviation Weather App
+This is a lightweight aviation weather app to enable a user to quickly look up TAF and METAR information for flight planning purposes. It utilizes the ADDS XML API for up-to-date flight weather information. 
+
+The Python Flask module will be used to develop the first iteration of this project. My long-term goal is to convert this to other platforms to expand my portfolio. 
+
+##Disclaimer
+This is being developed for personal use and academic purposes, but it not intended to replace certified flight planning software and tools. This application is not a substitute for the good judgement by the pilot-in-command and other flight crewmembers. 
+
+## Design Notes
+See the [Aviation Weather Center](https://www.aviationweather.gov/dataserver/example?datatype=metar) webpage for detailed information about their web API. 
--- wx_app.py
+++ wx_app.py
@@ -6,12 +6,13 @@ import xml.etree.ElementTree as ET
 # Default is Addison, Dallas, and McKinney 
 
 metarString = ' kads  f46 '
-tafString = ''
+tafString = 'kdal kftw'
 
-metarString = re.sub(" +", '%20', metarString)
+def processString(inputString):
+    # alternative to use Javascript for input validation
+    return re.sub(" +", '%20', inputString)
 
 def getWxRpt(metarString, tafString):
-
     urlMETAR = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={0}&hoursBeforeNow=1'.format(metarString)
     urlTAF = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString={0}&hoursBeforeNow=4'.format(tafString)
     rawResponse = requests.get(urlMETAR)
@@ -28,9 +29,10 @@ def getWxRpt(metarString, tafString):
                     dataMETAR[report[1].text] = report[0].text
                 else :
                     # If blank data found, will display an error
-                    # Reconsider designing a Try/Except statement
+                    # Redesign with a Try/Except statement
                     # Examine XML format for a bad query result
-                    dataMETAR[report[1].text] = report[1].text, "No data available or no data found for this station"
+                    dataMETAR[report[1].text] = report[1].text + "No data available or no data found for this station"
     return  dataMETAR.values(), dataTAF.values()
 
-print getWxRpt(metarString, tafString)
+metarString = processString(metarString)
+#print getWxRpt(metarString, tafString)
