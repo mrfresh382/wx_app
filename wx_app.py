6e07e9fad7514930d97ac5f96704bfdda4cdf9af
vagrant <vagrant@vagrant.vm>
2019-08-06 15:52:29 +0000

    Day 2 modifications

--- wx_app.py
+++ wx_app.py
@@ -1,18 +1,36 @@
 #!/usr/bin python
-import requests
+import requests, re
 import xml.etree.ElementTree as ET
 
 # Create a string to query NOAA weather server
 # Default is Addison, Dallas, and McKinney 
-urlMETAR = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=KADS%20KDAL%20KTKI&hoursBeforeNow=1'
-urlTAF = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString=KADS%20KDAL%20KTKI&hoursBeforeNow=4'
-rawResponse = requests.get(urlMETAR)
-xmlContent = rawResponse.content
-dataWX ={}
-root = ET.fromstring(xmlContent)
 
-for child in root:
-    if child.tag == 'data':
-        numResults = child.attrib['num_results']
-        for report in child.iter('METAR'):
-            print report[0].text
+metarString = ' kads  f46 '
+tafString = ''
+
+metarString = re.sub(" +", '%20', metarString)
+
+def getWxRpt(metarString, tafString):
+
+    urlMETAR = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={0}&hoursBeforeNow=1'.format(metarString)
+    urlTAF = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString={0}&hoursBeforeNow=4'.format(tafString)
+    rawResponse = requests.get(urlMETAR)
+    xmlContent = rawResponse.content
+    dataMETAR = {}
+    dataTAF = {}
+    root = ET.fromstring(xmlContent)
+
+    for child in root :
+        if child.tag == 'data' :
+            numResults = child.attrib['num_results']
+            for report in child.iter('METAR') :
+                if report[0].text :
+                    dataMETAR[report[1].text] = report[0].text
+                else :
+                    # If blank data found, will display an error
+                    # Reconsider designing a Try/Except statement
+                    # Examine XML format for a bad query result
+                    dataMETAR[report[1].text] = report[1].text, "No data available or no data found for this station"
+    return  dataMETAR.values(), dataTAF.values()
+
+print getWxRpt(metarString, tafString)
