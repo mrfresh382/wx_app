76cd3308c2e38422fca9100cc00e77b6a2ec0a56
vagrant <vagrant@vagrant.vm>
2019-08-09 15:40:02 +0000

    Worked on Post request in Flask app

--- wx_app.py
+++ wx_app.py
@@ -2,17 +2,15 @@
 import requests, re, os, sys, string
 import xml.etree.ElementTree as ET
 
-from flask import Flask, render_template, request, 
+from flask import Flask, render_template, request, \
     redirect, jsonify, url_for, flash
 
 from flask import session as login_session
 from flask import make_response
 
-# Create a string to query NOAA weather server
-# Default is Addison, Dallas, and McKinney 
-
-metarString = ' kads  f46 '
-tafString = 'kdal kftw'
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 APPLICATION_NAME = "Cheapo Weather App"
@@ -23,22 +21,19 @@ def processString(inputString):
     return re.sub(" +", '%20', inputString)
 
 def getWxRpt(metarString, tafString):
-    urlMETAR = 'https://www.aviationweather.gov/adds/dataserver_current/\
-        httpparam?dataSource=metars&requestType=retrieve&\
-        format=xml&stationString={0}&hoursBeforeNow=1'.format(metarString)
-    urlTAF = 'https://www.aviationweather.gov/adds/dataserver_current/\
-        httpparam?dataSource=tafs&requestType=retrieve&\
-        format=xml&stationString={0}&hoursBeforeNow=4'.format(tafString)
+    urlMETAR = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={0}&hoursBeforeNow=1'.format(metarString)
+    urlTAF = 'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=xml&stationString={0}&hoursBeforeNow=4'.format(tafString)
     rawResponse = requests.get(urlMETAR)
     xmlContent = rawResponse.content
     dataMETAR = {}
     dataTAF = {}
     root = ET.fromstring(xmlContent)
+    numResults = 0
 
     for child in root :
         if child.tag == 'data' :
             numResults = child.attrib['num_results']
-            if num_results == 0 :
+            if not numResults :
                 dataMETAR['No data found'] = 'Please try another query'
             for report in child.iter('METAR') :
                 if report[0].text :
@@ -51,19 +46,19 @@ def getWxRpt(metarString, tafString):
                         "No data available or no data found for this station"
     return  dataMETAR.values(), dataTAF.values()
 
-metarString = processString(metarString)
-#print getWxRpt(metarString, tafString)
-
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def showMainPage():
+    if request.method == 'POST':
+        results =  getWxRpt( processString(request.form['inputString']), processString(request.form['inputString']))
+        print results
     return render_template('mainpage.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/getData/<string:inputString>/', methods=['POST'])
 def getWeatherData():
-    print form.inputString.data
+    return ''
+
 # Example for redirects
 # redirect(url_for('methodName', arguments...))
-
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
